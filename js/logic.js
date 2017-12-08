dbc8b7a59585525d7c708e58dfb9a9bd1b1fd52d
Yaroslav Voloshyn <jarik.voloshyn@gmail.com>
2017-12-08 19:21:29 +0200

    first commit

---
+++ js/logic.js
@@ -0,0 +1,5 @@
+function nextPage(){
+	var nameEmail = document.getElementsByClassName('nameAndEmail'),
+		countryCity = document.getElementsByClassName('countryAndCity');
+	nameEmail.style.display = "none";
+}
