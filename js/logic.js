bf89b535abddfcbf1b7c87fe0be064362e4f8d36
Yaroslav Voloshyn <jarik.voloshyn@gmail.com>
2017-12-09 20:51:16 +0200

    third commit

--- js/logic.js
+++ js/logic.js
@@ -1,5 +1,33 @@
+var currPageNum = 1;
+
 function nextPage(){
-	var nameEmail = document.getElementsByClassName('nameAndEmail'),
-		countryCity = document.getElementsByClassName('countryAndCity');
-	nameEmail.style.display = "none";
+	if (currPageNum == 1){
+		var email = document.getElementsByName('email')[0].value;
+		if (email.search('@') > 0){
+			var elems = document.getElementsByClassName('nameAndEmail');
+			for (var i=0; i<elems.length; i++) elems[i].style.display='none';
+
+			var nextDiv = document.getElementsByClassName('countryCity');
+			for (var i=0; i<nextDiv.length; i++) nextDiv[i].style.display='block';
+
+			currPageNum += 1;
+		} else {
+			var forBord = document.getElementsByName('email');
+			for (var i=0; i<forBord.length; i++) forBord[i].style.border = "1px solid red";
+		}
+	}
+}
+function prevPage(){
+	if (currPageNum == 1) {
+		alert("You are on the first page!");
+	}
+	if (currPageNum == 2 ){
+		var elems = document.getElementsByClassName('nameAndEmail');
+		for (var i=0; i<elems.length; i++) elems[i].style.display='block';
+
+		var nextDiv = document.getElementsByClassName('countryCity');
+		for (var i=0; i<nextDiv.length; i++) nextDiv[i].style.display='none';
+
+		currPageNum -= 1;
+	}
 }
